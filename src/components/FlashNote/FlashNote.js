f6e2857dc9ff703ee3d81319a88276baf76440e0
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-04 09:43:55 +0300

    Update FlashNote.js
    Добавление разметки на страницу

--- src/components/FlashNote/FlashNote.js
+++ src/components/FlashNote/FlashNote.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
-FlashNote = () => {
-	render () {
-		return (
-
-		);
-	}
-} 
-
+let FlashNote = () => {
+	return (
+		<div className="FlashNote">
+			<div className="column" draggable="true">
+				<p className="column-header" contenteditable="true">
+					Заголовок 1
+				</p>
+				<div data-notes>
+					<div className="note" draggable="true">
+						Задача 1.1
+					</div>
+					<div className="note" draggable="true">
+						Задача 1.2
+					</div>
+					<div className="note" draggable="true">
+						Задача 1.3
+					</div>
+				</div>
+				<p className="column-footer">
+					<span className="action">
+						+ Добавить карточку
+					</span>
+				</p>
+			</div>
+		</div>
+	);
+}
 export default FlashNote;
