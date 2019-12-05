b2588a9a18b85868cd6e8f7e2b918b85102ddfc0
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-05 18:45:15 +0300

    Update Note.js
    Создание компоненты Note

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import clases from './Note.module.css';
 
-let Note = () => {
-	return (
-		<div className={clases.note} draggable="true" contenteditable="true">
-			
+class Note extends React.Component {
+
+
+	updateNote = (event) => {
+		console.log(3);
+	}
+	render() { 
+		let {id, title, message, deleteNote} = this.props;
+
+		return <div>
+			<div className={clases.note_wrap} draggable="true">
+				<div className={clases.note} contenteditable="true">
+					{message}
+				</div>
+				<button className={clases.note_delete} onClick={ () => deleteNote(id)}>
+					x
+				</button>
+			</div>
 		</div>
-	);
+	}	
 }
 
 export default Note;
