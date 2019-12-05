34904a9ff92004bfa91f6a2f5728be829fb3ace1
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-05 07:53:39 +0300

    Update App.js
    Добавление Route и BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import FlashNote from './components/FlashNote/FlashNote.js';
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="App">
-      <FlashNote />
-    </div>
-  );
+  	return (
+  		<BrowserRouter>
+		    <div className="App-wrapper">
+		    	<Route path=''
+		    			render={ () => <FlashNote /> }/>
+		    </div>
+		</BrowserRouter>
+ 	);
 }
 
 export default App;
