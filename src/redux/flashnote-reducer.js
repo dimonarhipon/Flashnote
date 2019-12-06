9cdf6119abbc117b70c8583b482aea0870e52454
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-06 14:03:15 +0300

    Update flashnote-reducer.js

--- src/redux/flashnote-reducer.js
+++ src/redux/flashnote-reducer.js
@@ -1,5 +1,14 @@
 import {combineReducers} from 'redux';
+import {load} from 'redux-localstorage-simple';
 import {ADD_NOTE, DELETE_NOTE, UPDATE_NOTE_TEXT, SET_NOTES} from './constans.js';
+
+let NOTES = load({ namespace: 'note-list'});
+
+if (!NOTES || !NOTES.note || !NOTES.note.length) {
+	NOTES = {
+		notes: [],
+	}
+}
 
 const rootReducer = combineReducers({});
 
@@ -11,23 +20,27 @@ const rootReducer = combineReducers({});
 // 	newNoteText: ''
 // };
 
-const NOTE = [
-	{
-		id: 1,
-		message: 'Learn 1',
-	},
-	{
-		id: 2,
-		message: 'Learn 2',
-	},
-	{
-		id: 3,
-		message: 'Learn 3',
-	},
-]
+// const NOTES = [
+// 	{
+// 		id: 1,
+// 		message: 'Learn 1',
+// 	},
+// 	{
+// 		id: 2,
+// 		message: 'Learn 2',
+// 	},
+// 	{
+// 		id: 3,
+// 		message: 'Learn 3',
+// 	},
+// ]
+export const deleteNote = id => ({
+	type: DELETE_NOTE, 
+	id,
+})
 
 
-const flashnoteReducer = (state = NOTE, {id, message, type}) => {
+const flashnoteReducer = (state = NOTES.notes, {id, message, type}) => {
 	switch (type) {
 		case ADD_NOTE: 
 			return [
@@ -36,6 +49,8 @@ const flashnoteReducer = (state = NOTE, {id, message, type}) => {
 					message,
 				}	
 			];
+		case DELETE_NOTE: 
+			return [...state].filter(note => note.id !== id);
 		default:
 			return state;
 	}
