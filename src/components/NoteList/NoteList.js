4b233004cd77650aa1dec01508030a0e65ffe1a0
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-06 14:03:11 +0300

    Update NoteList.js

--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -4,20 +4,22 @@ import PropTypes from 'prop-types';
 import Note from '../Note/Note.js';
 
 
-const NoteList = ({ notesList }) => (
+const NoteList = ({ notesList,  deleteNote}) => (
 	<ul className="note-list">
 		{notesList.map(( {id, message }) => (
-			<Note key={id} message={message} />
+			<Note key={id} message={message} deleteNote={deleteNote} id={id}/>
 		))}
 	</ul>
 )
 
 NoteList.propTypes = {
 	notesList: PropTypes.array,
+	deleteNote: PropTypes.func,
 }
 
 NoteList.defaultProps = {
 	notesList: [],
+	deleteNote: () => {}
 }
 
 export default NoteList;
