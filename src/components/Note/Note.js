cf6dcff64716323ed08d637df6444051ce54db30
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-06 13:12:59 +0300

    Update Note.js

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import clases from './Note.module.css';
+import PropTypes from 'prop-types';
 
-class Note extends React.Component {
+// class Note extends React.Component {
 
 
-	updateNote = (event) => {
-		console.log(3);
-	}
-	render() { 
-		let {id, title, message, deleteNote} = this.props;
+// 	updateNote = (event) => {
+// 		console.log(3);
+// 	}
+// 	render() { 
+// 		let {id, title, message, deleteNote} = this.props;
 
-		return <div>
-			<div className={clases.note_wrap} draggable="true">
-				<div className={clases.note} contenteditable="true">
-					{message}
-				</div>
-				<button className={clases.note_delete} onClick={ () => deleteNote(id)}>
-					x
-				</button>
-			</div>
-		</div>
-	}	
+// 		return <div>
+// 			<div className={clases.note_wrap} draggable="true">
+// 				<div className={clases.note} contenteditable="true">
+// 					{message}
+// 				</div>
+// 				<button className={clases.note_delete} onClick={ () => deleteNote(id)}>
+// 					x
+// 				</button>
+// 			</div>
+// 		</div>
+// 	}	
+// }
+
+const Note = ({ message }) => (
+	<li className="note-item">
+		<span>{message}</span>
+	</li>
+)
+
+Note.propTypes = {
+	message: PropTypes.string,
 }
+Note.defaultProps = {
+	message: '',
+}
+
 
 export default Note;
