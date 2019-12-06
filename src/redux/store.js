6d9372374ef0b24cbd16107baaa78700af9c5054
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2019-12-06 13:13:10 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,32 @@
-const notes = [
-	{id: 1, title: 'Заметка №1', message: 'Это текст заметки №1' },
-	{id: 2, title: 'Заметка №2', message: 'Это текст заметки №2' },
-	{id: 3, title: 'Заметка №3', message: 'Это текст заметки №3' },
-	{id: 4, title: 'Заметка №4', message: 'Это текст заметки №4' },
-	{id: 5, title: 'Заметка №5', message: 'Это текст заметки №5' },
-	{id: 6, title: 'Заметка №6',  message: 'Это текст заметки №6' },
-	{id: 7, title: 'Заметка №7',  message: 'Это текст заметки №7' },
-	{id: 8, title: 'Заметка №8',  message: 'Это текст заметки №8' },
-];
 
-let pageSize: 5;
-let totalUserCount: 20;
-let currentPage: 2;
 
-export default notes;
+import {createStore} from 'redux'; 
+import rootReducer from './flashnote-reducer.js'; 
+
+const configureStore = preloadedState => (
+	createStore (
+		rootReducer,
+		preloadedState,
+	)
+);
+
+const store = configureStore({});
+
+export default store;
+
+// const notes = [
+// 	{id: 1, message: 'Это текст заметки №1' },
+// 	{id: 2, message: 'Это текст заметки №2' },
+// 	{id: 3, message: 'Это текст заметки №3' },
+// 	{id: 4, message: 'Это текст заметки №4' },
+// 	{id: 5, message: 'Это текст заметки №5' },
+// 	{id: 6,  message: 'Это текст заметки №6' },
+// 	{id: 7,  message: 'Это текст заметки №7' },
+// 	{id: 8,  message: 'Это текст заметки №8' },
+// ];
+
+// let pageSize: 5;
+// let totalUserCount: 20;
+// let currentPage: 2;
+
+// export default notes;
