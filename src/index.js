fcf8803779f916dfb0d5a04a5f2300ae40bbd795
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-01-19 12:18:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./redux/redux-store";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/redux-store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store="store">
+    <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
