1858f94bdebbef39add4dc17c421b91fc0a69ab3
dimonarhipon <40203705+dimonarhipon@users.noreply.github.com>
2020-01-20 14:22:22 +0300

    Update Note.js

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -5,17 +5,31 @@ let Note = props => {
   debugger;
   return (
     <div>
-      <div className={clases.note_wrap} id={props.id}>
+      <div className={clases.note_wrap}>
         <button
           className={clases.note_delete}
           onClick={() => {
-            props.removeNode(props.id);
+            props.deleteNote(props.id);
           }}
         >
           Х
         </button>
         <h2 className={clases.note}>{props.title}</h2>
-        <div className={clases.note}>{props.text}</div>
+        {props.editMode ? (
+          <input
+            id={props.id}
+            className={clases.note}
+            type="text"
+            onBlur={props.deactiveEditMode}
+            onChange={() => props.updateText(props.id)}
+            // value={props.text}
+          />
+        ) : (
+          <div className={clases.note} onDoubleClick={props.activeEditMode}>
+            {props.text}
+          </div>
+        )}
+        {/* <input className={clases.note} value={props.text} /> */}
       </div>
     </div>
   );
